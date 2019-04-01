e4f1132579cda927dbed9c13fbc58112a458b072
martinbedouret <martinbedouret@gmail.com>
2019-04-01 18:44:23 -0300

    test

--- test/basicAuth.spec.ts
+++ test/basicAuth.spec.ts
@@ -5,6 +5,7 @@ describe('Basic auth', () => {
     it('works if username and password are passed in the url', () => {
         browser.url('http://admin:admin@the-internet.herokuapp.com/basic_auth');
 
+
         expect(BasicAuthPage.message).to.eq('Congratulations! You must have the proper credentials.');
     });
 });
